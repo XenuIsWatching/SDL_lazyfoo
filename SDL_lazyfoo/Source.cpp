4096c3bdb61e0f7494b8f4ae35e5bec235c19bdb
Ryan McClelland <rmcclelland@uico.com>
2017-02-27 13:59:23 -0600

    Texture Loading and Rendering

--- SDL_lazyfoo/Source.cpp
+++ SDL_lazyfoo/Source.cpp
@@ -1,11 +1,12 @@
 /*This source code copyrighted by Lazy Foo' Productions (2004-2015)
 and may not be redistributed without written permission.*/
 
-//Using SDL, SDL_image, standard IO, and strings
+//Using SDL, SDL_image, standard IO, math, and strings
 #include <SDL.h>
 #include <SDL_image.h>
 #include <stdio.h>
 #include <string>
+#include <cmath>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
@@ -20,17 +21,14 @@ bool loadMedia();
 //Frees media and shuts down SDL
 void close();
 
-//Loads individual image
-SDL_Surface* loadSurface(std::string path);
+//Loads individual image as texture
+SDL_Texture* loadTexture( std::string path );
 
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
 
-//The surface contained by the window
-SDL_Surface* gScreenSurface = NULL;
-
-//Current displayed PNG image
-SDL_Surface* gPNGSurface = NULL;
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
 
 bool init()
 {
@@ -38,33 +36,47 @@ bool init()
     bool success = true;
 
     //Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
-        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
         success = false;
     }
     else
     {
-        //Create window
-        gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (gWindow == NULL)
+        //Set texture filtering to linear
+        if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
         {
-            printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+            printf( "Warning: Linear texture filtering not enabled!" );
+        }
+
+        //Create window
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
             success = false;
         }
         else
         {
-            //Initialize PNG loading
-            int imgFlags = IMG_INIT_PNG;
-            if (!(IMG_Init(imgFlags) & imgFlags))
+            //Create renderer for window
+            gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+            if( gRenderer == NULL )
             {
-                printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+                printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
                 success = false;
             }
             else
             {
-                //Get window surface
-                gScreenSurface = SDL_GetWindowSurface(gWindow);
+                //Initialize renderer color
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+                //Initialize PNG loading
+                int imgFlags = IMG_INIT_PNG;
+                if( !( IMG_Init( imgFlags ) & imgFlags ) )
+                {
+                    printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+                    success = false;
+                }
             }
         }
     }
@@ -77,75 +89,66 @@ bool loadMedia()
     //Loading success flag
     bool success = true;
 
-    //Load PNG surface
-    gPNGSurface = loadSurface("06_extension_libraries_and_loading_other_image_formats/loaded.png");
-    if (gPNGSurface == NULL)
-    {
-        printf("Failed to load PNG image!\n");
-        success = false;
-    }
-
+    //Nothing to load
     return success;
 }
 
 void close()
 {
-    //Free loaded image
-    SDL_FreeSurface(gPNGSurface);
-    gPNGSurface = NULL;
-
-    //Destroy window
-    SDL_DestroyWindow(gWindow);
+    //Destroy window    
+    SDL_DestroyRenderer( gRenderer );
+    SDL_DestroyWindow( gWindow );
     gWindow = NULL;
+    gRenderer = NULL;
 
     //Quit SDL subsystems
     IMG_Quit();
     SDL_Quit();
 }
 
-SDL_Surface* loadSurface(std::string path)
+SDL_Texture* loadTexture( std::string path )
 {
-    //The final optimized image
-    SDL_Surface* optimizedSurface = NULL;
+    //The final texture
+    SDL_Texture* newTexture = NULL;
 
     //Load image at specified path
-    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
-    if (loadedSurface == NULL)
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if( loadedSurface == NULL )
     {
-        printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
     }
     else
     {
-        //Convert surface to screen format
-        optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, NULL);
-        if (optimizedSurface == NULL)
+        //Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+        if( newTexture == NULL )
         {
-            printf("Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+            printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
         }
 
         //Get rid of old loaded surface
-        SDL_FreeSurface(loadedSurface);
+        SDL_FreeSurface( loadedSurface );
     }
 
-    return optimizedSurface;
+    return newTexture;
 }
 
-int main(int argc, char* args[])
+int main( int argc, char* args[] )
 {
     //Start up SDL and create window
-    if (!init())
+    if( !init() )
     {
-        printf("Failed to initialize!\n");
+        printf( "Failed to initialize!\n" );
     }
     else
     {
         //Load media
-        if (!loadMedia())
+        if( !loadMedia() )
         {
-            printf("Failed to load media!\n");
+            printf( "Failed to load media!\n" );
         }
         else
-        {
+        {   
             //Main loop flag
             bool quit = false;
 
@@ -153,23 +156,45 @@ int main(int argc, char* args[])
             SDL_Event e;
 
             //While application is running
-            while (!quit)
+            while( !quit )
             {
                 //Handle events on queue
-                while (SDL_PollEvent(&e) != 0)
+                while( SDL_PollEvent( &e ) != 0 )
                 {
                     //User requests quit
-                    if (e.type == SDL_QUIT)
+                    if( e.type == SDL_QUIT )
                     {
                         quit = true;
                     }
                 }
 
-                //Apply the PNG image
-                SDL_BlitSurface(gPNGSurface, NULL, gScreenSurface, NULL);
+                //Clear screen
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+                SDL_RenderClear( gRenderer );
 
-                //Update the surface
-                SDL_UpdateWindowSurface(gWindow);
+                //Render red filled quad
+                SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0x00, 0x00, 0xFF );        
+                SDL_RenderFillRect( gRenderer, &fillRect );
+
+                //Render green outlined quad
+                SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
+                SDL_SetRenderDrawColor( gRenderer, 0x00, 0xFF, 0x00, 0xFF );        
+                SDL_RenderDrawRect( gRenderer, &outlineRect );
+                
+                //Draw blue horizontal line
+                SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0xFF, 0xFF );        
+                SDL_RenderDrawLine( gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2 );
+
+                //Draw vertical line of yellow dots
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0x00, 0xFF );
+                for( int i = 0; i < SCREEN_HEIGHT; i += 4 )
+                {
+                    SDL_RenderDrawPoint( gRenderer, SCREEN_WIDTH / 2, i );
+                }
+
+                //Update screen
+                SDL_RenderPresent( gRenderer );
             }
         }
     }
